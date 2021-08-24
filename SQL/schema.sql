df444c3d2e34d0855f6f188ef012eb5440b8f414
krkarizs <sevenke@gmail.com>
2021-08-24 16:18:02 +0300

    Function for retrieving data and SQL schema

---
+++ SQL/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE stockinfo (
+    date DATE PRIMARY KEY,
+    index FLOAT(3),
+    minTemperature FLOAT(3),
+    maxTemperature FLOAT(3),
+    rain FLOAT(3)
+);
---
+++ func2/main.py
@@ -0,0 +1,70 @@
+import sqlalchemy
+import json
+import decimal, datetime
+
+#Connection info for the database
+connection_name = "" #Connection name from the details page of the SQL instance
+table_name = "stockinfo" #The name of the table in the database
+db_name = "helsinkistock" #The name of our database
+db_user = "postgres"
+db_password = "" #Password for the postgres database
+
+driver_name = 'postgres+pg8000'
+query_string =  dict({"unix_sock": "/cloudsql/{}/.s.PGSQL.5432".format(connection_name)})
+
+#JSON encoder function for SQLAlchemy special classes (date and float)
+def alchemyencoder(obj):
+    if isinstance(obj, datetime.date):
+        return obj.isoformat()
+    elif isinstance(obj, decimal.Decimal):
+        return float(obj)
+
+#Function 1 - selecting the stock info and the temperature
+def select_temp(request):
+
+    stmt = sqlalchemy.text("SELECT date, index, maxTemperature FROM {} ORDER BY maxTemperature".format(table_name))
+    
+    db = sqlalchemy.create_engine(
+      sqlalchemy.engine.url.URL(
+        drivername=driver_name,
+        username=db_user,
+        password=db_password,
+        database=db_name,
+        query=query_string,
+      ),
+      pool_size=5,
+      max_overflow=2,
+      pool_timeout=30,
+      pool_recycle=1800
+    )
+    try:
+        with db.connect() as conn:
+          res = conn.execute(stmt)
+          return json.dumps([dict(r) for r in res], default=alchemyencoder)
+    except Exception as e:
+        return 'Error: {}'.format(str(e))
+
+#Function 2 - selecting the stock info and rain(mm)
+def select_rain(request):
+
+    stmt = sqlalchemy.text("SELECT date, index, rain FROM {} ORDER BY rain".format(table_name))
+    
+    db = sqlalchemy.create_engine(
+      sqlalchemy.engine.url.URL(
+        drivername=driver_name,
+        username=db_user,
+        password=db_password,
+        database=db_name,
+        query=query_string,
+      ),
+      pool_size=5,
+      max_overflow=2,
+      pool_timeout=30,
+      pool_recycle=1800
+    )
+    try:
+        with db.connect() as conn:
+          res = conn.execute(stmt)
+          return json.dumps([dict(r) for r in res], default=alchemyencoder)
+    except Exception as e:
+        return 'Error: {}'.format(str(e))
